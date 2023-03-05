bab9a4f6a680330d25a1c93f88f6f87bfd34db56
Ion Stici <ionsticidev@gmail.com>
2023-03-05 17:40:32 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Officelite coming soon site solution
+
+This is a solution to the [Officelite coming soon site challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/officelite-coming-soon-site-M4DIPNz8g).
---
+++ src/index.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/index.css';
+
+function App() {
+    return null;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+
+// // // // // // // // // //
+
+// index.js
+// A simple solution to complex tasks is coming soon
+
+// Say goodbye to inefficient juggling of multiple apps, teams, and projects. Officelite is the new
+// collaboration platform built with an intuitive interface to improve productivity.
+
+// Get Started
+
+// Basic
+// Free
+// Up to 5 users for free
+// Basic document collaboration
+// 2 gb storage
+// Great security and support
+// Try for Free
+
+// Pro
+// $9.99
+// Per user, billed monthly
+// All essential integrations
+// 50 gb storage
+// More control and insights
+// Try for Free
+
+// Ultimate
+// $19.99
+// Per user, billed monthly
+// Robust work management
+// 100 gb storage
+// VIP support
+// Try for Free
+
+// <!-- If you're choosing to make this date dynamic, set it to 30 days in the future from the moment the visitor views the page -->
+// Coming 4 Nov 2020
+
+// <!-- If you're choosing to make this timer dynamic, have it countdown from the date you set above -->
+// 47 Days
+// 07 Hours
+// 56 Min
+// 14 Sec
+// Get Started
+
+// // // // // // // // // // // // // // // // // // // //
+
+// sign up html
+// Work smarter. Save time.
+
+// Easily manage your projects. Get on the list and receive in-app perks available only to
+// early subscribers. We are moving into final development and getting ready for official launch soon.
+
+// <!-- If you're choosing to make this date dynamic, set it to 30 days in the future from the moment the visitor views the page -->
+// Coming 4 Nov 2020
+
+// <!-- If you're choosing to make this timer dynamic, have it countdown from the date you set above -->
+// 47 Days
+// 07 Hours
+// 56 Min
+// 14 Sec
+
+// Name
+// Email Address
+// Basic Pack Free
+// Basic Pack Free
+// Pro Pack $9.99
+// Ultimate Pack $19.99
+// Phone Number
+// Company
+// Get on the list
+
+// // // // // // // // // //
