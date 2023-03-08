6f2c0339f630471d167a6fc4f0f81f1a21065851
Ion Stici <ionsticidev@gmail.com>
2023-03-08 14:33:01 +0200

    Add timer logic, fix sign-up layout bug

--- src/store.js
+++ src/store.js
@@ -1,1 +1,17 @@
- 
+let future = new Date().setDate(new Date().getDate() + 30);
+
+const timer = setInterval(() => {
+    let now = new Date().getTime();
+    let time = future - now;
+
+    let day = Math.floor(time / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((time % (1000 * 60)) / 1000);
+
+    // console.log(day, hours, minutes, seconds);
+
+    if (time === 0) {
+        clearInterval(timer);
+    }
+}, 1000);
